262e1cae299bf2a78c44b049bf218d9836af21fa
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-05-22 11:38:59 -0600

    example Item

--- RandomItems/main.m
+++ RandomItems/main.m
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
 
 int main(int argc, const char * argv[])
 {
@@ -26,6 +27,12 @@ int main(int argc, const char * argv[])
         for (NSString *item in items) {
             NSLog(@"%@", item);
         }
+        
+        BNRItem *item = [[BNRItem alloc] init];
+        [item setItemName:@"Red Sofa"];
+        [item setSerialNumber:@"A1B2C"];
+        [item setValueInDollars:100];
+        NSLog(@"%@ %@ %@ %d", [item itemName], [item dateCreated], [item serialNumber], [item valueInDollars]);
         
         items = nil;
     }
