1850ca0ffc64ed2feac9033be286e735fcd393c8
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-05-22 09:57:08 -0600

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Sandquist, Cassandra G on 5/22/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
