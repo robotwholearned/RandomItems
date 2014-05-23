6bae268598e3c2b42293cd7fb40241e09898fc7f
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-05-23 08:09:01 -0600

    properties

--- RandomItems/BNRItem.h
+++ RandomItems/BNRItem.h
@@ -9,31 +9,13 @@
 #import <Foundation/Foundation.h>
 
 @interface BNRItem : NSObject {
-    NSString* _itemName;
-    NSString* _serialNumber;
-    int _valueInDollars;
-    NSDate* _dateCreated;
-
-    BNRItem* _containedItem;
-    __weak BNRItem* _container;
 }
-
-- (void)setItemName:(NSString*)str;
-- (NSString*)itemName;
-
-- (void)setSerialNumber:(NSString*)str;
-- (NSString*)serialNumber;
-
-- (void)setValueInDollars:(int)v;
-- (int)valueInDollars;
-
-- (NSDate*)dateCreated;
-
-- (void)setContainedItem:(BNRItem*)item;
-- (BNRItem*)containedItem;
-
-- (void)setContainer:(BNRItem*)item;
-- (BNRItem*)container;
+@property (nonatomic, copy) NSString* itemName;
+@property (nonatomic, copy) NSString* serialNumber;
+@property (nonatomic) int valueInDollars;
+@property (nonatomic, readonly, strong) NSDate* dateCreated;
+@property (nonatomic, strong) BNRItem* containedItem;
+@property (nonatomic, weak) BNRItem* container;
 
 //desiganted initializer
 - (instancetype)initWithItemName:(NSString*)name
